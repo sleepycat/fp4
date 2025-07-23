ceffb735b8c81d9ad4a80d9216ad2da5c5fc813d
Mike Williamson <mike@korora.ca>
2025-07-23 16:31:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FP3
+This is the FP3 application.
+It is split into two main parts; a backend api and a frontend React application.
---
+++ frontend/rsbuild.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    // use a custom template to address A11y and SEO issues.
+    template: './static/index.html',
+  },
+  output: {
+    // This will prevent .LICENSE.txt files from being generated
+    legalComments: 'none',
+  },
+  performance: {
+    preload: {
+      type: 'all-assets',
+    },
+  },
+});
