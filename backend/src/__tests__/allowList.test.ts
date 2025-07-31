ca74d4a74f47f876e2bfb421618b040d3e30fe4e
Mike Williamson <mike@korora.ca>
2025-07-31 10:03:38 -0400

    Improve allowlist testing.

--- backend/src/__tests__/allowList.test.ts
+++ backend/src/__tests__/allowList.test.ts
@@ -4,9 +4,19 @@ import { expect } from "jsr:@std/expect"
 import { allowList } from "../allowList.ts"
 
 describe("allowList", () => {
-  it("works", () => {
-    const isAllowed = allowList("example.com,example.ca")
-    const result = isAllowed("foo@example.com")
-    expect(result).toEqual(true)
+  describe("given a comma separated list of domains", () => {
+    describe("when passed an email whose domain is included in the list", () => {
+      it("returns true ", () => {
+        const isAllowed = allowList("example.com,example.ca")
+        const result = isAllowed("foo@example.com")
+        expect(result).toEqual(true)
+      })
+    })
+    describe("when passed an email whose domain is not included", () => {
+      it("returns false", () => {
+        const isAllowed = allowList("example.com,example.ca")
+        expect(isAllowed("foo@bad.com")).toBeFalsy()
+      })
+    })
   })
 })
