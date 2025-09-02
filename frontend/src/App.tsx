572d744c110b857bc46053b66afbe34ef0fc5b4d
Mike Williamson <mike@korora.ca>
2025-09-02 11:08:03 -0400

    Add goc signature block to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css';
+import { Flag, SVG } from "./Signature.tsx";
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with Overused Grotesk</h1>
+    <div className="content" style={{ justifySelf: "center" }}>
+      <section
+        style={{ display: "flex" }}
+      >
+        <SVG>
+          <title>Canadian Flag</title>
+          <Flag style={{ fill: "#ea2d37" }} />
+        </SVG>
+        <span
+          style={{
+            paddingLeft: "0.8em",
+            lineHeight: "1em",
+            textAlignLast: "left",
+          }}
+        >
+          Government of<br /> Canada
+        </span>
+      </section>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
