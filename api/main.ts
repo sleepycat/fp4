d3b0dadff676521c01c808ef0dc2294f32b02d74
Mike Williamson <mike@korora.ca>
2025-10-31 09:27:25 -0400

    Simplify intialization code for env vars

--- api/main.ts
+++ api/main.ts
@@ -83,48 +83,25 @@ export function createContext(
   }
 }
 
+function getEnv(key: string): string {
+  const value = Deno.env.get(key)
+  if (value === undefined) {
+    throw new Error(`The ${key} environmental variable must be defined.`)
+  }
+  return value
+}
+
 // Functional core, imperative shell pattern:
-// Read from the environment here in the entry point,
+// Read from the environment here in the application entry point,
 // not elsewhere deeper in the application.
-const ALLOWED_DOMAINS = Deno.env.get("ALLOWED_DOMAINS")
-if (ALLOWED_DOMAINS === undefined) {
-  // fail loudly
-  throw new Error("The ALLOWED_DOMAINS environmental variable must be defined.")
-}
-const NOTIFY_API_KEY = Deno.env.get("NOTIFY_API_KEY")
-if (NOTIFY_API_KEY === undefined) {
-  throw new Error("The NOTIFY_API_KEY envrionmental variable must be defined.")
-}
-const NOTIFY_TEMPLATE_ID = Deno.env.get("NOTIFY_TEMPLATE_ID")
-if (NOTIFY_TEMPLATE_ID === undefined) {
-  throw new Error(
-    "The NOTIFY_TEMPLATE_ID envrionmental variable must be defined.",
-  )
-}
-const JWT_SECRET = Deno.env.get("JWT_SECRET")
-if (JWT_SECRET === undefined || JWT_SECRET == "") {
-  throw new Error(
-    "The JWT_SECRET envrionmental variable must be defined.",
-  )
-}
-const JWT_ISSUER = Deno.env.get("JWT_ISSUER")
-if (JWT_ISSUER === undefined || JWT_ISSUER == "") {
-  throw new Error(
-    "The JWT_ISSUER envrionmental variable must be defined.",
-  )
-}
-const DB_PATH = Deno.env.get("DB_PATH")
-if (DB_PATH === undefined || DB_PATH == "") {
-  throw new Error(
-    "The DB_PATH envrionmental variable must be defined.",
-  )
-}
-
-// Reasonable defaults:
-const PORT = Deno.env.get("PORT") || 3000
+const DB_PATH = getEnv("DB_PATH")
+const jwtSecret = getEnv("JWT_SECRET")
+const jwtIssuer = getEnv("JWT_ISSUER")
+const allowedDomains = getEnv("ALLOWED_DOMAINS")
+const notifyApiKey = getEnv("NOTIFY_API_KEY")
+const notifyTemplateId = getEnv("NOTIFY_TEMPLATE_ID")
+const PORT = Deno.env.get("PORT") || "3000"
 const HOST = Deno.env.get("HOST") || "0.0.0.0"
-
-// Env vars should not be read anywhere else in the app.
 
 // Dependency Injection for database functions allows for easy testing.
 export const db: DatabaseSync = new DatabaseSync(DB_PATH)
@@ -133,11 +110,11 @@ const server = Server({
   schema,
   context: createContext({
     db,
-    jwtSecret: JWT_SECRET,
-    jwtIssuer: JWT_ISSUER,
-    allowedDomains: ALLOWED_DOMAINS,
-    notifyApiKey: NOTIFY_API_KEY,
-    notifyTemplateId: NOTIFY_TEMPLATE_ID,
+    jwtSecret,
+    jwtIssuer,
+    allowedDomains,
+    notifyApiKey,
+    notifyTemplateId,
   }),
 })
 
