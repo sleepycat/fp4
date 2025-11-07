e37cb1bdf493763966ac74bb292676e73cd6e61b
Mike Williamson <mike@korora.ca>
2025-11-07 13:47:04 -0500

    Add an actual test for the schema.

--- api/src/__tests__/schema.test.ts
+++ api/src/__tests__/schema.test.ts
@@ -1,8 +1,45 @@
-import { describe, it } from "jsr:@std/testing/bdd"
-import { expect } from "jsr:@std/expect"
+import { describe, it } from "@std/testing/bdd"
+import { expect } from "@std/expect"
+import { graphql } from "graphql"
+import { schema } from "../schema.ts"
+import type { Context } from "../types/Context.ts"
+import type { CookieStore } from "@whatwg-node/cookie-store/"
 
-describe("testing", () => {
-  it("works", () => {
-    expect(true).toEqual(true)
+// Augment the global Request interface
+declare global {
+  interface Request {
+    cookieStore?: CookieStore
+  }
+}
+
+describe("graphql schema", () => {
+  describe("query.seizures", () => {
+    describe("given an authenticated user in context", () => {
+      const mockContext = {
+        authenticatedUser: { user_id: 1, email: "test@example.com" },
+        db: {
+          getSeizures: () => ({
+            err: false,
+            results: undefined,
+          }),
+        },
+      } as unknown as Context
+
+      it("returns a list of seizures", async () => {
+        const results = await graphql({
+          schema,
+          source: `
+            {
+              seizures {
+                substance
+                amount
+              }
+            }
+          `,
+          contextValue: mockContext,
+        })
+        expect(results).toEqual({ data: { seizures: null } })
+      })
+    })
   })
 })
