d5774e8a0ed71bc2e7ed877ffab951df493b5bd1
Mike Williamson <mike@korora.ca>
2025-11-07 15:16:58 -0500

    Add test for unauthenticate user to seizures

--- api/src/__tests__/schema.test.ts
+++ api/src/__tests__/schema.test.ts
@@ -33,5 +33,41 @@ describe("graphql schema", () => {
         expect(results).toEqual({ data: { seizures: null } })
       })
     })
+
+    describe("without an authenticated user", () => {
+      const mockContext = {
+        db: {
+          // no authenticatedUser object
+          getSeizures: () => ({
+            err: false,
+            results: undefined,
+          }),
+        },
+      } as unknown as Context
+
+      it("returns a list of seizures", async () => {
+        const results = await graphql({
+          schema,
+          source: `
+            {
+              seizures {
+                substance
+                amount
+              }
+            }
+          `,
+          contextValue: mockContext,
+        })
+
+        expect(results).toEqual({
+          data: { seizures: null },
+          errors: [
+            expect.objectContaining({
+              message: expect.stringMatching(/Authentication required/),
+            }),
+          ],
+        })
+      })
+    })
   })
 })
